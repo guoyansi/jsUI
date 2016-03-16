29974f8c67dc9ee09fa3163f002e97a865d5790d
guoyansi <1012447978@qq.com>
2016-03-16 16:02:30 +0800

    第一次提交

---
+++ scrollbar/mobiscrool-ui.js
@@ -0,0 +1,96 @@
+//(function(){
+function scrollbar(){
+	var scrollbar=$("#wrapper").attr("scrollbar");
+	if(scrollbar=="no"){
+		$("#wrapper").attr("scrollbar","yes");
+	}else{
+		return;
+	}
+	
+	var myScroll,
+		pullDownEl, pullDownOffset,
+		pullUpEl, pullUpOffset,
+		generatedCount = 0;
+	
+	/**
+	 * 下拉刷新 （自定义实现此方法）
+	 * myScroll.refresh();		// 数据加载完成后，调用界面更新方法
+	 */
+	function pullDownAction () {
+		console.log("下拉刷新");
+		myScroll.refresh();
+	}
+	
+	/**
+	 * 滚动翻页 （自定义实现此方法）
+	 * myScroll.refresh();		// 数据加载完成后，调用界面更新方法
+	 */
+	function pullUpAction () {
+		console.log("上拉加载更多");
+		myScroll.refresh();
+	}
+	
+	/**
+	 * 初始化iScroll控件
+	 */
+	//this.loaded=function(){
+	function loaded() {
+		pullDownEl = document.getElementById('pullDown');
+		pullDownOffset = pullDownEl.offsetHeight;
+		pullUpEl = document.getElementById('pullUp');	
+		pullUpOffset = pullUpEl.offsetHeight;
+		
+		myScroll = new iScroll('wrapper', {
+			scrollbarClass: 'myScrollbar', /* 重要样式 */
+			useTransition: false, /* 此属性不知用意，本人从true改为false */
+			topOffset: pullDownOffset,
+			onRefresh: function () {
+				if (pullDownEl.className.match('loading')) {
+					pullDownEl.className = '';
+					pullDownEl.querySelector('.pullDownLabel').innerHTML = '下拉刷新...';
+				} else if (pullUpEl.className.match('loading')) {
+					pullUpEl.className = '';
+					pullUpEl.querySelector('.pullUpLabel').innerHTML = '上拉加载更多...';
+				}
+			},
+			onScrollMove: function () {
+				if (this.y >= 5 && !pullDownEl.className.match('flip')) {
+					pullDownEl.className = 'flip';
+					pullDownEl.querySelector('.pullDownLabel').innerHTML = '松手开始更新...';
+					this.minScrollY = 0;
+				} else if (this.y < 5 && pullDownEl.className.match('flip')) {
+					pullDownEl.className = '';
+					pullDownEl.querySelector('.pullDownLabel').innerHTML = '下拉刷新...';
+					this.minScrollY = -pullDownOffset;
+				} else if (this.y <= (this.maxScrollY - 5) && !pullUpEl.className.match('flip')) {
+					pullUpEl.className = 'flip';
+					pullUpEl.querySelector('.pullUpLabel').innerHTML = '松手开始更新...';
+					this.maxScrollY = this.maxScrollY;
+				} else if (this.y > (this.maxScrollY + 5) && pullUpEl.className.match('flip')) {
+					pullUpEl.className = '';
+					pullUpEl.querySelector('.pullUpLabel').innerHTML = '上拉加载更多...';
+					this.maxScrollY = pullUpOffset;
+				}
+			},
+			onScrollEnd: function () {
+				if (pullDownEl.className.match('flip')) {
+					pullDownEl.className = 'loading';
+					pullDownEl.querySelector('.pullDownLabel').innerHTML = '加载中...';				
+					pullDownAction();
+				} else if (pullUpEl.className.match('flip')) {
+					pullUpEl.className = 'loading';
+					pullUpEl.querySelector('.pullUpLabel').innerHTML = '加载中...';				
+					pullUpAction();
+				}
+			}
+		});
+		
+		setTimeout(function () { document.getElementById('wrapper').style.left = '0'; }, 800);
+	}
+	//初始化绑定iScroll控件 
+	document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+	//document.addEventListener('DOMContentLoaded', loaded, false);
+	loaded();
+}
+//})();
+
